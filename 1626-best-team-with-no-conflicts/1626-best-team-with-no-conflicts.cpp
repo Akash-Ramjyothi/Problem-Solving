f38898296add6424ecd51a2a81a629834e4cf76d
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2023-02-01 01:13:20 +0530

    Time: 296 ms (48.87%), Space: 23.4 MB (33.01%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int bestTeamScore(vector<int>& scores, vector<int>& ages) {
+        vector<vector<int>> v;
+        int n=scores.size();
+        for(int i=0;i<n;i++){
+            v.push_back({-ages[i],-scores[i]});
+        }
+        sort(v.begin(),v.end());
+        vector<int> dp(n,0);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j>=0;j--){
+                if(-v[i][1]<=-v[j][1]){
+                    dp[i]=max(dp[i],dp[j]-v[i][1]);
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+        return ans;
+    }
+};
